d17cdc8c90e11009575dc3ad242c4ea6d9c582a8
07Sean <97667393+07Sean@users.noreply.github.com>
2023-02-18 18:46:50 +0800

    TIM

--- pages/Chatroom/Chatroom.js
+++ pages/Chatroom/Chatroom.js
@@ -1,66 +1,44 @@
-// pages/Chatroom/Chatroom.js
+import TIM from '../../TUIKit/lib/tim-wx-sdk';
+import { genTestUserSig }  from '../../TUIKit/debug/GenerateTestUserSig';
+import TIMUploadPlugin from '../../TUIKit/lib/tim-upload-plugin';
+import TIMProfanityFilterPlugin from '../../TUIKit/lib/tim-profanity-filter-plugin';
+
 Page({
-
-    /**
-     * 页面的初始数据
-     */
     data: {
-
+        config: {
+            userID: '1', //User ID
+            SDKAPPID: 1400793258, // Your SDKAppID
+            SECRETKEY: 'dd041b31c2053b81a32ebd59ec845faaacc2bf19c7bd5317e7df110c09215b73', // Your secretKey
+            EXPIRETIME: 604800,
+        }
     },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad(options) {
-
+    onLoad() {
+        const userSig = genTestUserSig(this.data.config).userSig 
+        wx.$TUIKit = TIM.create({
+            SDKAppID: this.data.config.SDKAPPID
+        })
+        wx.$chat_SDKAppID = this.data.config.SDKAPPID;
+        wx.$chat_userID = this.data.config.userID;
+        wx.$chat_userSig = userSig;
+        wx.$TUIKitTIM = TIM;
+        wx.$TUIKit.registerPlugin({ 'tim-upload-plugin': TIMUploadPlugin });
+        wx.$TUIKit.registerPlugin({ 'tim-profanity-filter-plugin': TIMProfanityFilterPlugin });
+        wx.$TUIKit.login({
+            userID: this.data.config.userID,
+            userSig
+        });
+        wx.setStorage({
+            key: 'currentUserID',
+            data: [],
+        });
+        wx.$TUIKit.on(wx.$TUIKitTIM.EVENT.SDK_READY, this.onSDKReady,this);
     },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
     onUnload() {
-
+        wx.$TUIKit.off(wx.$TUIKitTIM.EVENT.SDK_READY, this.onSDKReady,this);
     },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh() {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom() {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage() {
-
+    onSDKReady() {
+        const TUIKit = this.selectComponent('#TUIKit');
+        TUIKit.init();
     }
-})
+});
